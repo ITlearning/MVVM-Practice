30a5a7bea96fb9563f80462738d3aac103687fb8
IT Learning <yo7504@kakao.com>
2021-09-03 21:15:32 +0900

    Initial Commit

---
+++ MVVM-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Practice
+//
+//  Created by IT learning on 2021/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
